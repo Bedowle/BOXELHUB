281928af4dc33eaac6ccd34aeb34ed1811258ca9
Bedowle <damha2000rap@gmail.com>
2025-11-20 13:44:56 +0100

    Add Categories component with navigation links

---
+++ components/Categories.js
@@ -0,0 +1,12 @@
+export default function Categories() {
+  return (
+    <nav className="categories">
+      <a href="#">Arte</a>
+      <a href="#">Hogar</a>
+      <a href="#">Videojuegos</a>
+      <a href="#">Juguetes</a>
+      <a href="#">Moda</a>
+      <a href="#">Sorpréndeme</a>
+    </nav>
+  )
+}
