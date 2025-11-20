22abb60cefc9278a4f3ae439d67083ae3b8ea350
Bedowle <damha2000rap@gmail.com>
2025-11-20 13:18:04 +0100

    Create Maker component for 3D printing services

---
+++ pages/maker.js
@@ -0,0 +1,8 @@
+export default function Maker() {
+  return (
+    <div className="container">
+      <h1>Área de Maker</h1>
+      <p>Aquí podrás ofrecer tus servicios de impresión 3D, subir diseños y gestionar pedidos.</p>
+    </div>
+  )
+}
