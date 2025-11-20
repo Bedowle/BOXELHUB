80e74cf58bd7673252fea09273a7b24830667dd7
Bedowle <damha2000rap@gmail.com>
2025-11-20 01:07:36 +0100

    Create Home component for VoxelHub marketplace

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1>VOXELHUB</h1>
+      <p>Marketplace de impresión 3D on-demand</p>
+      <div className="actions">
+        <button>Entrar como Cliente</button>
+        <button>Entrar como Maker</button>
+      </div>
+    </>
+  );
+}
