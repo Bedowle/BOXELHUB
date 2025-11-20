3628b071f86482b83504d017e7b4d2ad630af2af
Bedowle <damha2000rap@gmail.com>
2025-11-20 13:21:35 +0100

    Update cliente.js

--- pages/cliente.js
+++ pages/cliente.js
@@ -1,8 +1,8 @@
 export default function Cliente() {
   return (
-    <div className="container">
+    <>
       <h1>Área de Cliente</h1>
       <p>Aquí podrás explorar proyectos de impresión 3D, hacer pedidos y gestionar tus compras.</p>
-    </div>
+    </>
   )
 }
