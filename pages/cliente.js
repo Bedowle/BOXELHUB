03426c89aba8730fdd3db4ac010bdd18dbf20944
Bedowle <damha2000rap@gmail.com>
2025-11-20 23:50:54 +0100

    Delete pages directory

--- pages/cliente.js
+++
@@ -1,8 +0,0 @@
-export default function Cliente() {
-  return (
-    <>
-      <h1>Área de Cliente</h1>
-      <p>Aquí podrás explorar proyectos de impresión 3D, hacer pedidos y gestionar tus compras.</p>
-    </>
-  )
-}
--- pages/maker.js
+++
@@ -1,8 +0,0 @@
-export default function Maker() {
-  return (
-    <>
-      <h1>Área de Maker</h1>
-      <p>Aquí podrás ofrecer tus servicios de impresión 3D, subir diseños y gestionar pedidos.</p>
-    </>
-  )
-}
