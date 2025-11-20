73b69993955487338f4933815a1fa8563e872df2
Bedowle <damha2000rap@gmail.com>
2025-11-20 13:43:24 +0100

    Add Navbar and Categories components to App

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,17 @@
 import '../styles/globals.css'
+import Navbar from '../components/Navbar'
+import Categories from '../components/Categories'
 
 export default function App({ Component, pageProps }) {
   return (
-    <main>
-      <div className="container">
-        <Component {...pageProps} />
-      </div>
-    </main>
+    <>
+      <Navbar />
+      <Categories />
+      <main>
+        <div className="container">
+          <Component {...pageProps} />
+        </div>
+      </main>
+    </>
   )
 }
