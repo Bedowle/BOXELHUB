8645ac2e98e92eb66c166d4ec4a047d93d9c6bdd
Bedowle <damha2000rap@gmail.com>
2025-11-20 01:30:22 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <div className="container">
-      <h1>Test de estilos</h1>
-      <p>¿Se aplica el fondo y el diseño?</p>
+    <>
+      <h1>VOXELHUB</h1>
+      <p>Marketplace de impresión 3D on-demand</p>
       <div className="actions">
-        <button>Botón 1</button>
-        <button>Botón 2</button>
+        <button>Entrar como Cliente</button>
+        <button>Entrar como Maker</button>
       </div>
-    </div>
+    </>
   );
 }
