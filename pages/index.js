909944ac72a1b8556c826668b61b7f99afaa6a17
Bedowle <damha2000rap@gmail.com>
2025-11-20 13:18:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <>
       <h1>VOXELHUB</h1>
       <p>Marketplace de impresión 3D on-demand</p>
       <div className="actions">
-        <button>Entrar como Cliente</button>
-        <button>Entrar como Maker</button>
+        <Link href="/cliente">
+          <button>Entrar como Cliente</button>
+        </Link>
+        <Link href="/maker">
+          <button>Entrar como Maker</button>
+        </Link>
       </div>
     </>
-  );
+  )
 }
