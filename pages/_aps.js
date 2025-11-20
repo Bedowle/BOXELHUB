3ac87fce94801cc924e5c5a80b8cec007a1980b6
Bedowle <damha2000rap@gmail.com>
2025-11-20 01:10:52 +0100

    Wrap Component in main and container div

--- pages/_aps.js
+++ pages/_aps.js
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <div className="container">
+        <Component {...pageProps} />
+      </div>
+    </main>
+  )
 }
