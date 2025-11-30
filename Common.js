16632989409d2ce53880e9e57d0a6d2b3b7d296c
Bedowle <damha2000rap@gmail.com>
2025-11-30 02:12:21 +0100

    Delete Common.js

--- Common.js
+++
@@ -1,9 +0,0 @@
-// prisma.config.cjs
-module.exports = {
-  schema: "prisma/schema.prisma",
-  datasource: {
-    provider: "sqlite",
-    url: "file:./prisma/dev.db",
-  },
-  engine: "classic",
-};
